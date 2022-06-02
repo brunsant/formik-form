fc564e70c61c3d0ffe3ceb9c7e56712d6b049f95
brunsant <brunadossantosaraujo@Brunas-MBP.lan>
2022-06-02 13:21:28 +0200

    Created invoce component and uncrontrolled form inputs

--- src/components/RootComponent.js
+++ src/components/RootComponent.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SignInComponent from "./SignInComponent";
+import SignUpComponent from "./SignUpComponent";
+import CreateInvoiceComponent from "./CreateInvoiceComponent";
 
 class RootComponent extends React.Component {
   render() {
@@ -8,6 +10,12 @@ class RootComponent extends React.Component {
       <BrowserRouter>
         <Routes>
           <Route exact path={"/signin"} element={<SignInComponent />} />
+          <Route exact path={"/signup"} element={<SignUpComponent />} />
+          <Route
+            exact
+            path={"/create_invoice"}
+            element={<CreateInvoiceComponent />}
+          />
         </Routes>
       </BrowserRouter>
     );
